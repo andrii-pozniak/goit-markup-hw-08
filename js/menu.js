f1a0870456fcb85ea083cb8e00c3fade5df2e695
andrii-pozniak <103370189+andrii-pozniak@users.noreply.github.com>
2022-06-18 14:44:31 +0300

    add change to mobile menu

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,14 @@
 (() => {
-    const refs = {
-    openModalBtn: document.querySelector("[data-menu-open]"),
-    closeModalBtn: document.querySelector("[data-menu-close]"),
-    modal: document.querySelector("[data-menu]"),
-  };
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-open");
-  } 
-    
-  
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
 })();
